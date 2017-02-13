8e4106601d414d0daa2c7b706643825002a757e7
Marijo Horvat <Marijo.Horvat@vsite.hr>
2017-02-13 20:48:42 +0100

    PlayerCharacter made PlayerCharacter Movement PlayerCharacter boundry Box Test EnemyCharacter Test EnemyCharacter movement
    yea, forgot to commit earlier...

--- Dungeon/Form1.Designer.cs
+++ Dungeon/Form1.Designer.cs
@@ -29,11 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.playerChar = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.enemy01 = new System.Windows.Forms.PictureBox();
+            this.gamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playerChar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Controls.Add(this.enemy01);
+            this.gamePanel.Controls.Add(this.playerChar);
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(1313, 766);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // playerChar
+            // 
+            this.playerChar.BackColor = System.Drawing.Color.Maroon;
+            this.playerChar.Location = new System.Drawing.Point(369, 472);
+            this.playerChar.Name = "playerChar";
+            this.playerChar.Size = new System.Drawing.Size(90, 96);
+            this.playerChar.TabIndex = 0;
+            this.playerChar.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // enemy01
+            // 
+            this.enemy01.BackColor = System.Drawing.Color.Red;
+            this.enemy01.Location = new System.Drawing.Point(690, 238);
+            this.enemy01.Name = "enemy01";
+            this.enemy01.Size = new System.Drawing.Size(20, 50);
+            this.enemy01.TabIndex = 1;
+            this.enemy01.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1313, 766);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
+            this.Text = "Dungeon";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.gamePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.playerChar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy01)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.PictureBox playerChar;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox enemy01;
     }
 }
 
--- Dungeon/Form1.cs
+++ Dungeon/Form1.cs
@@ -12,9 +12,68 @@ namespace Dungeon
 {
     public partial class Form1 : Form
     {
+        private bool moveRight;
+        private bool moveLeft;
+        private bool moveUp;
+        private bool moveDown;
+        private Random randomGenerator;
+        int speed = 5;
+        int enemySpeed = 20;
+
         public Form1()
         {
             InitializeComponent();
+            timer.Enabled = true;                           // start the timer (game)
+            this.FormBorderStyle = FormBorderStyle.None;    // remove boarders
+            this.TopMost = true;                            // bring frame to frnt
+            this.Bounds = Screen.PrimaryScreen.Bounds;      // game in full screen
+            randomGenerator = new Random();
+            playerChar.Top = Screen.PrimaryScreen.Bounds.Height - playerChar.Height;
+            playerChar.Left = Screen.PrimaryScreen.Bounds.Size.Width / 2 - playerChar.Width;
+            EnemyInitalPosition();
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (playerChar.Left > gamePanel.Left) { if (moveLeft == true) { playerChar.Left -= speed; } }
+            if (playerChar.Right < gamePanel.Right) { if (moveRight == true) { playerChar.Left += speed; } }
+            if (playerChar.Top > gamePanel.Top) {  if (moveUp == true) { playerChar.Top -= speed; } }
+            if (playerChar.Bottom < gamePanel.Bottom) { if (moveDown == true) { playerChar.Top += speed; } }
+            if (enemy01.Top >= 0 && enemy01.Top != Screen.PrimaryScreen.Bounds.Height)
+            {
+                enemy01.Top += enemySpeed;
+                if (enemy01.Top > Screen.PrimaryScreen.Bounds.Height) { EnemyInitalPosition(); }
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            MovePlayer(sender, e, 15, true);
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            MovePlayer(sender, e, 5, false);
+        }
+
+        private void MovePlayer(object sender, KeyEventArgs e, int currentSpeed, bool choice)
+        {
+            if (e.KeyCode == Keys.ShiftKey) { speed = currentSpeed; }
+            if (e.KeyCode == Keys.Right) { moveRight = choice; }
+            if (e.KeyCode == Keys.Left) { moveLeft = choice; }
+            if (e.KeyCode == Keys.Up) { moveUp = choice; }
+            if (e.KeyCode == Keys.Down) { moveDown = choice; }
+        }
+
+
+        private void EnemyInitalPosition()
+        {
+            enemy01.Top = 0;
+            int randomNumber = randomGenerator.Next(0, Screen.PrimaryScreen.Bounds.Size.Width);
+            enemy01.Left = randomNumber; 
         }
     }
 }
